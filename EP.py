a03b05add7d74c9ad6661d5f94ec0c0fd733127a
PortoSamuel <38635893+PortoSamuel@users.noreply.github.com>
2018-04-24 15:24:14 -0300

    até o Item 4 polido

--- EP.py
+++ EP.py
@@ -10,9 +10,9 @@ with open('arquivo.txt','r') as arquivo:
     conteudo=arquivo.read()
     estoque=json.loads(conteudo)
 
-continua=True
+continuacao=True
 
-while continua:
+while continuacao:
     print('''
           Controle de estoque
           0-sair
@@ -25,51 +25,76 @@ while continua:
           ''' 
           )
 
-    escolha=int(input('Faça sua escolha: '))
+    escolha=int(input('Faça sua escolha:'))
 
     if escolha==0:
+        continuacao=False
         print('Até um outro dia')
-        continua=False
+
+    
+    elif escolha>6 or escolha<0:
+        print('Operação inválida!')        
 
     elif escolha==1:
+        continua=True
         nome=input('Nome do produto: ')    
         if nome in estoque:
-            print('Produto já cadastrado')        
-        else:
-            valor=float(input('Valor unitário em reais: '))
-            if valor<0:
-                print('O preço não pode ser negativo!')
-            else:
-                quantidade=int(input('Quantidade inicial: '))
-                if quantidade<0:
-                    print('A quantidade inicial não pode ser negativa!')
+            print('Produto já cadastrado')
+            continua=False
+        while continua:
+                valor=float(input('Valor unitário em reais: '))
+                if valor<0:
+                    print('O preço não pode ser negativo!')
                 else:
-                    estoque[nome]=quantidade,valor
+                    break
+        while continua:
+            quantidade=int(input('Quantidade inicial: '))
+            if quantidade<0:
+                print('A quantidade inicial não pode ser negativa!')
+            else:
+                estoque[nome]=quantidade,valor
+                print('{0} adicionado por {1} reais a unidade' .format(nome,estoque[nome][1]))
+                break
+            
                 
     elif escolha==2:
-        nome=input('Nome do produto: ')
-        if nome not in estoque:
-            print('Elemento não encontrado')
-        else:
-            del estoque[nome]
+        while True:
+            nome=input('Nome do produto (0 para sair): ')
+            if nome=='0':
+                break
+            elif nome not in estoque:
+                print('Elemento não encontrado!')
+            else:
+                del estoque[nome]
             
     elif escolha==3:
-        nome=input('Nome do produto: ')
-        if nome not in estoque:
-            print('Elemento não encontrado')
-        else:
-            valor=float(input('Novo valor unitário em reais: '))
-            if valor<0:
-                print('O preço não pode ser negativo!')
+        while True:
+            nome=input('Nome do produto (0 para sair): ')
+            if nome=='0':
+                break
+            elif nome not in estoque:
+                print('Elemento não encontrado!')
             else:
-                quantidade=int(input('Alteração na quantidade: '))
+                while True:
+                    valor=float(input('Valor unitário em reais (0 para manter o preço): '))
+                    if valor == 0:
+                        break
+                    elif valor<0:
+                        print('O preço não pode ser negativo!')
+                    else:
+                        estoque[nome][1]=valor
+                        print(estoque[nome][1])
+                        break
+                quantidade=int(input('Alteração na quantidade (0 para não alterar o estoque): '))
                 variavel=estoque[nome][0]
-                estoque[nome][0]=variavel+quantidade
-                estoque[nome][1]=valor                
+                estoque[nome][0]=variavel+quantidade                
                 print('Novo estoque de {0}: {1} unidades, {2} reais cada' .format(nome,estoque[nome][0],estoque[nome][1]))
-    
+                break
+            
     elif escolha==4:
-        print(estoque)
+        print('Os produtos no estoque são:')
+        for i in estoque:
+            print('{0}: {1} itens, preço: {2} a unidade' .format(i,estoque[i][0],estoque[i][1]))
         
     elif escolha==5:
         lista=[]
